9139018fe6dac309537704521f1cfed6b2a00217
Chris Saunders <54923165+cjgsaunders@users.noreply.github.com>
2025-10-17 12:45:43 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
          name: "smartSummariesFeature",
          url: "https://github.com/cjgsaunders/testpackage/releases/download/2/smartSummariesFeature.xcframework.zip",
          //url: "https://github.com/sky-uk/smart-summaries-kmp/releases/download/1/smartSummariesFeature.xcframework.zip",
-         checksum:"b996b6e433fc20a9b7959546fb395c6927fbf1f6bedc3f859377095ea4aa2251")
+         checksum:"94f610b864115b57fac82f5860e60fc2e0699056fdfac8f9b9eb5abb69e868e7")
    //  .binaryTarget(
    //          name: "smartSummariesFeature",
    //          path: "smartSummariesFeature.xcframework.zip"
